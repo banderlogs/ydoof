b1ebf3f7005746c629ab330f02025913a14549df
Nikita Pankratov <nick.pankratov@virtualinstruments.com>
2016-11-05 18:35:05 -0700

    Models.py cleanup and fixes

--- apps/foody/models.py
+++ apps/foody/models.py
@@ -1,6 +1,5 @@
 from __future__ import unicode_literals
 from django.db import models
-import bcrypt
 import re
 import datetime
 
@@ -8,7 +7,7 @@ now = datetime.datetime.now()
 EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
 
 
-class UserManager(models.Manager):
+class BuyerManager(models.Manager):
     def logUser(self, username_in, pwd_in):
         error = False
         if len(username_in) < 2:
@@ -20,7 +19,7 @@ class UserManager(models.Manager):
         if error is True:
             return False
         else:
-            passwordHash = Users.UserManager.get(username=username_in).password.encode()
+            passwordHash = Buyer.BuyerManager.get(username=username_in).password.encode()
             if bcrypt.hashpw(pwd_in, passwordHash) == passwordHash:
                 return True
             else:
@@ -45,7 +44,7 @@ class UserManager(models.Manager):
             return [False, error]
         else:
             hashedPW = bcrypt.hashpw(pwd_up, bcrypt.gensalt())
-            newuser = Users.UserManager.create(username=username_up, email=email_up, password=hashedPW, created_at=now)
+            newuser = Buyer.BuyerManager.create(username=username_up, email=email_up, password=hashedPW, created_at=now)
             return [True]
 
     def validuser(self, username_valid):
@@ -90,7 +89,7 @@ class Order(models.Manager):
     state = models.CharField(
         max_length=2,
         choices=STATE_CHOICES,
-        default=NO_STATE,
+        default='NO_STATE',
     )
 
     comment = models.TextField()
@@ -98,14 +97,15 @@ class Order(models.Manager):
     updated_at = models.DateTimeField(auto_now=True)
 
     DELIVERY_CHOICES = (
+        ('NS', 'NO_STATE'),
         ('CO', 'CARRY_OUT'),
         ('DE', 'DELIVERY'),
         ('BO', 'BOTH')
     )
-    state = models.CharField(
+    delivery_type = models.CharField(
         max_length=2,
         choices=DELIVERY_CHOICES,
-        default=CARRY_OUT,
+        default='NO_STATE',
     )
 
 
@@ -114,25 +114,25 @@ class Buyer(models.Model):
     address = models.CharField(max_length=100)
     email = models.CharField(max_length=45)
     password = models.CharField(max_length=25)
-    fav_dishes = models.ManyToManyField(Dishes)
-    fav_chefs = models.ManyToManyField(Chefs)
+    fav_dishes = models.ManyToManyField(Dish)
+    fav_chefs = models.ManyToManyField(Chef)
     credit_card = models.CharField(max_length=150)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    buyer_manager = UserManager()
+    buyer_manager = BuyerManager()
+
 
 class Message(models.Model):
-    user_name_sender = models.OneToOne(
+    user_name_sender = models.OneToOneField(
         Buyer,
-        on_delete = models.CASCADE
+        on_delete=models.CASCADE
     )
-    user_name_reciever = models.OneToOne(
+    user_name_reciever = models.OneToOneField(
         Chef,
-        on_delete = models.CASCADE,
+        on_delete=models.CASCADE,
     )
     send_at = models.DateTimeField(auto_now_add=True)
     message = models.TextField()
-
 
 
 class Chef(models.Model):
@@ -145,13 +145,10 @@ class Chef(models.Model):
     rating = models.FloatField(null=True, blank=True, default='5.0')
     photo = models.URLField(max_length=200)
     location = models.CharField(max_length=100)
-    messages = models.OneToMany(
-        Message,
-        on_delete = models.CASCADE,
-    )
+
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    chef_manager = UserManager()
+    chef_manager = BuyerManager()
 
 
 class Dish(models.Model):
@@ -160,7 +157,8 @@ class Dish(models.Model):
     rating = models.FloatField(null=True, blank=True, default='5.0')
     description = models.TextField()
     photo = models.URLField(max_length=200)
+    belong_to_chef = models.ForeignKey(Chef)
+
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    belong_to_chef = models.OneToMany(Chefs)
-    dish_manager = UserManager()
+    dish_manager = BuyerManager()
