1ef3a2e468ba1ea108e7a83e8131fb4c7c8a88ef
Roman Basinschi <Sleppy1992@gmail.com>
2016-11-05 22:51:03 -0700

    Nikita's views

--- apps/foody/views.py
+++ apps/foody/views.py
@@ -1,7 +1,46 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .models import Dish
 
 # Create your views here.
 
 
 def index(request):
+    context = {
+        "img": 123
+    }
+
     return render(request, "index.html")
+
+
+def add_dish(request):
+    """
+    Author: NP
+    Used to add a dish
+
+    :param request:
+    :return:
+    """
+    if request.POST:
+        dish_name = request.POST.get("dish_name")
+        dish_description = request.POST.get("dish_description")
+        dish_price = request.POST.get("dish_description")
+        dish_photo_url = request.POST.get("dish_photo_url")
+        dish_chef_obj = request.POST.get("dish_chef_obj")
+
+        dish_err = Dish.dish_manager.create_new_dish(dish_name, dish_description,
+                                                     dish_price, dish_photo_url,
+                                                     dish_chef_obj)
+
+        # If create_new_dish does not return an error
+        if dish_err[0]:
+
+            return render()
+        else:
+            messages.error(request, dish_err[1])
+
+            return redirect('/add_dish')
+    elif request.GET:
+        return render(request, '/add_dish')
+
+def return_dish(request):
